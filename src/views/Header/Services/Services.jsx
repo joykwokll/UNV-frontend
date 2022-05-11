8160eedd2d0d50b2ec1b982586469c43d3144db1
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-05-11 13:00:02 +0800

    react-dropdown updated

--- src/views/Header/Services/Services.jsx
+++ src/views/Header/Services/Services.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import { Button } from "react-bootstrap";
 import Dropdown from "react-dropdown";
+// import Dropdown from "react-bootstrap/Dropdown"
 import "react-dropdown/style.css";
 
 const options = ["Anti Aging", "Pigmentation", "Sensetive Skin", "Pimple"];
@@ -15,6 +16,13 @@ function Services(props) {
         value={defaultOption}
         placeholder="Services"
       />
+      
+
+      {/* <Dropdown.Menu show>
+        <Dropdown.Header>Services</Dropdown.Header>
+        <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+        <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+      </Dropdown.Menu> */}
     </div>
   );
 }
