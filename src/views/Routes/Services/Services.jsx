be34ab27cc2d41e8979d687e83b34263003ac184
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-05-11 17:38:08 +0800

    removed style.css

--- src/views/Routes/Services/Services.jsx
+++ src/views/Routes/Services/Services.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-// import { Button } from "react-bootstrap";
-import Dropdown from "react-dropdown";
-// import Dropdown from "react-bootstrap/Dropdown"
-import "react-dropdown/style.css";
 
 const options = ["Anti Aging", "Pigmentation", "Sensetive Skin", "Pimple"];
 const defaultOption = options[0];
